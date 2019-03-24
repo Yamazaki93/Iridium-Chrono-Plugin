8387336fdd4b400afdef8f40cbd10de0de482fd6
Michael Lu <38804680+Yamazaki93@users.noreply.github.com>
2019-03-24 15:30:29 -0700

    Added detection and update due date on updating tasks

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const chrono = require('chrono-node');
 const moment = require('moment');
 module.exports.Initialize = (iridium) => {
     iridium.TaskLists.on(iridium.TaskEvents.TaskCreated, (taskData) => {
-        let parsed = chrono.parse(taskData.title, new Date(), { forwardDate: true });
-        if (parsed.length && parsed[0].start) {
-            let dateObj = moment(parsed[0].start.date());
-            iridium.TaskLists.SetTaskDueDate(taskData.listId, taskData.id, dateObj.format('YYYY-MM-DD'));
-        }
+        setDueDateBasedOnTaskData(iridium, taskData);
     });
+    iridium.TaskLists.on(iridium.TaskEvents.TaskUpdated, (taskData) => {
+        setDueDateBasedOnTaskData(iridium, taskData);
+    });
+}
+
+function setDueDateBasedOnTaskData(iridium, taskData) {
+    let parsed = chrono.parse(taskData.title, new Date(), { forwardDate: true });
+    if (parsed.length && parsed[0].start) {
+        let dateObj = moment(parsed[0].start.date());
+        iridium.TaskLists.SetTaskDueDate(taskData.listId, taskData.id, dateObj.format('YYYY-MM-DD'));
+    }
 }
